8dd4bc5d0b85fbd48742cdee592b788faa7e157e
Kevin Choi <kchoi117@gmail.com>
2017-09-29 16:15:08 -0400

    first commit

---
+++ app/src/main/java/com/kevin/pmc_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kevin.pmc_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final Intent popUp = new Intent(MainActivity.this, Pop.class);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(popUp);
+
+    }
+}
---
+++ app/src/main/java/com/kevin/pmc_app/Pop.java
@@ -0,0 +1,37 @@
+package com.kevin.pmc_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Kevin on 9/29/2017.
+ */
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popwindow);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * 0.7), (int)(height * 0.7));
+
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load a file
+            }
+        });
+    }
+
+}
